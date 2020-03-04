f5ccde6c4e10d537dab4035127ecb70f9ddd1e02
vagrant <vagrant@vagrant.vm>
2020-03-04 01:14:30 +0000

    test pokeapi calls

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from jinja2 import StrictUndefined
+from pprint import pprint
+import requests, json
+
+app = Flask(__name__)
+
+# Flask Sessions and Debug Toolbar
+app.secret_key = 'omnislash'
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def home():
+    return 'work in progress'
+
+url = 'https://pokeapi.co/api/v2/pokemon/'
+pokemon_api = requests.get(url)
+pprint(pokemon_api.json())
+
+
+if __name__ == "__main__":
+    app.debug = True
+
+
+    app.run(host="0.0.0.0")
