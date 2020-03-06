bfa678799d749b557e4c23217318dccea73f0260
vagrant <vagrant@vagrant.vm>
2020-03-06 02:13:01 +0000

    additional pokeapi calls

--- server.py
+++ server.py
@@ -16,14 +16,20 @@ def home():
 
 # Test calls
 # Pokemon info is accessed by a pokemon's number. Ex: bulbasaur == 1
-url = 'https://pokeapi.co/api/v2/pokemon/1'
+url = 'https://pokeapi.co/api/v2/pokemon/1/'
 pokemon_api = requests.get(url)
+
+# Front pokemon sprite image url
+pprint(pokemon_api.json()['sprites']['front_default'])
 
 # Pokemon name
 pprint(pokemon_api.json()['name'])
 
-# Front pokemon sprite image url
-# pprint(pokemon_api.json()['sprites']['front_default'])
+
+# Pokemon types (fire, water, grass, etc)
+pokemon_types = pokemon_api.json()['types']
+for types in pokemon_types:
+    pprint(types['type']['name'])
 
 
 
